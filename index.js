59e33e37f5845a07d8550a1d4c699b35cd16d6c8
ka-di-ma-hh <junewanja8@gmail.com>
2024-07-21 01:37:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^a-zA-Z0-9-'\s]+/g, '')
+  }
+
+  static titleize(string){
+    let noCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+    let arr = string.split(' ')
+    let newArr = []
+
+    for(let i in arr){
+      if(i==0){
+        newArr.push(this.capitalize(arr[i]))
+      }
+      else{
+        if(noCap.includes(arr[i])){
+        newArr.push(arr[i])
+        }
+        else{
+          newArr.push(this.capitalize(arr[i]))
+        }
+      
+    }
+  }
+  return newArr.join(" ")
+
+  }
 }
